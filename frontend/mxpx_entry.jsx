c55aac1897efe30abc149488d921fc84ea90f4bc
Msheezi <marshall.randolph@gmail.com>
2019-07-03 18:54:15 -0700

    Add HashRouter, refactor entryfile

--- frontend/mxpx_entry.jsx
+++ frontend/mxpx_entry.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Root from './components/root'
 import configureStore from './store/store'
-import * as SessionAPIUtil from './util/session_api_util'
 
 
 document.addEventListener("DOMContentLoaded", () => {
-const root = document.getElementById('root')
-const store = configureStore()
-// window.login = SessionAPIUtil.login
-// window.logout = SessionAPIUtil.logout
-// window.signup = SessionAPIUtil.signup
-window.getState = store.getState;
-window.dispatch = store.dispatch
+    const store = configureStore()
+    
+    window.getState = store.getState;
+    window.dispatch = store.dispatch
+    
+    
+    const root = document.getElementById('root')
 
-ReactDOM.render(<h1>React Is Working</h1>, root)
+ReactDOM.render(<Root store={store}/>, root)
 })
