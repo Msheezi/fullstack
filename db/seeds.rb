b3d329f4e798923b85ef3284099bab527b230068
Msheezi <marshall.randolph@gmail.com>
2019-07-09 17:39:28 -0700

    Add transaction to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do 
+
+
 
 # Demo Account User
-User.create(username:"demo", password:"password",email:"demo@demo.com",first_name:"demo",last_name:"demo")
+User.destroy_all
+user1 = User.create!(username:"demo", password:"password",email:"demo@demo.com",first_name:"demo",last_name:"demo")
 
+Category.destroy_all
+cat1 = Category.create!(title:"Nature")
+cat2 = Category.create!(title:"Architecture")
+cat3 = Category.create!(title:"People")
+cat4 = Category.create!(title:"Animals")
+cat5 = Category.create!(title:"Food")
+cat6 = Category.create!(title:"Black and White")
 
-Category.new(title:"Nature")
-Category.new(title:"Architecture")
-Category.new(title:"People")
-Category.new(title:"Animals")
-Category.new(title:"Food")
-Category.new(title:"Black and White")
-
-
+end
 # user=User.new(email:"chris@korsk.com", password: "111111", band_name: "Kors K",
 # description: "Kors K is a legend in the bemani scene. With over 20 years of making a variety of music, you'll recognize his signature beats anywhere.")
 # path = File.join(Rails.root, 'app', "assets", "images","duetomorrow.jpg")
