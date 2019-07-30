1e9372e7782daf24219610dd4c56b9d386352076
Msheezi <marshall.randolph@gmail.com>
2019-07-30 14:05:45 -0700

    Update Post Show styling

--- frontend/components/homefeed/post_index_item.jsx
+++ frontend/components/homefeed/post_index_item.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const PostIndexItem = ( { post, deletePost, props }) => {
+const PostIndexItem = ({ post, deletePost, props }) => {
+  return (
+    <li className="photo-post-index">
+      <img className="photo-post-index-photo" src={post.photoUrl} />
+      {/* <p>{state.entities.users[post.author_id].first_name}</p> */}
+      <br />
+      <Link to={`/posts/${post.id}`} className="photo-index-title">
+        {post.title}
+      </Link>
 
+      <br />
 
-  return   (
-        <li className='photo-post-index'>
-        {/* debugger */}
-        <img className='photo-post-index-photo' src={post.photoUrl}/>
-        {/* <p>{state.entities.users[post.author_id].first_name}</p> */}
-        <br/>
-        <Link to={`/posts/${post.id}`} className="photo-index-title">{post.title}</Link>
-        
-        <br/>
-
-        {/* <button onClick={(e)=> deletePost(post.id, e).then(()=>props.fetchPosts())}>Delete</button> */}
+      {/* <button onClick={(e)=> deletePost(post.id, e).then(()=>props.fetchPosts())}>Delete</button> */}
     </li>
-);
-}
-
+  );
+};
 
 export default PostIndexItem;
--- frontend/components/posts/post_show.jsx
+++ frontend/components/posts/post_show.jsx
@@ -12,9 +12,12 @@ class PostShow extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="post-show-container">
         <img src={this.props.post.photoUrl} className="post-show-img" />
         <h3 className="post-show-title">{this.props.post.title}</h3>
+        <h2 className="post-show-author">by Author</h2>
+        <p className="post-create-time">post time</p>
+        <p className="post-category">category</p>
       </div>
     );
   }
