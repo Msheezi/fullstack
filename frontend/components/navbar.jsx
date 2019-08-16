4885a7d081a89034ec0c9a019c0b18f9829cbb42
Msheezi <marshall.randolph@gmail.com>
2019-08-16 14:00:07 -0700

    Update base styling on post sub modal

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,70 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Dashboard from './posts/modal_form'
+import React from "react";
+import { Link } from "react-router-dom";
+import Dashboard from "./posts/modal_form";
 
-
-// const setNavClass = ({currentUser})=> {
-//     let navClass
-//     if (currentUser !== null) {
-//         navClass = "nav-logged"
-//     } else if (currentUser === null) {
-//         navClass = "nav-not-logged"
-//     }
-//     return navClass
-// }
-     
-
-const NavBar = ({currentUser, logout}) => { 
-   
-
-    const sessionLinks = () => (
-        <span className="login-buttons">
-            <Link to="/login" id="login-btn" >Login</Link>
-            <Link to="/signup" className="login-buttons-btn" >Sign Up</Link>
-            {/* <button className="login-buttons-btn" onClick={() => this.props.history.push("/signin")}>Log in</button>
+const NavBar = ({ currentUser, logout }) => {
+  const sessionLinks = () => (
+    <span className="login-buttons">
+      <Link to="/login" id="login-btn">
+        Login
+      </Link>
+      <Link to="/signup" className="login-buttons-btn">
+        Sign Up
+      </Link>
+      {/* <button className="login-buttons-btn" onClick={() => this.props.history.push("/signin")}>Log in</button>
             <button className="login-buttons-btn" onClick={() => this.props.history.push("/signup")}>Sign Up</button> */}
-
-        </span>
-    );
-    const personalGreeting = () => (
-        <span className='login-buttons'>
-            {/* <Link to="/post/new">Add A New Photo</Link> */}
-            <Dashboard/>
-            <h3 className="header-name">Hi, {currentUser.username}!</h3> 
-            <button className="login-buttons-btn" onClick={logout}>Log Out</button>
-        </span >
-    )
-    let loginButtons = currentUser ? personalGreeting() : sessionLinks();
-        
-     return (
-<div className={(currentUser) ? "nav-logged": "nav-not-logged"}>
-    <span className='logo'>   
-        <h3 id="logo"><Link to="/" id="logo">MYpx</Link></h3>
     </span>
+  );
+  const personalGreeting = () => (
+    <span className="login-buttons">
+      {/* <Link to="/post/new">Add A New Photo</Link> */}
+      <Dashboard />
+      <h3 className="header-name">Hi, {currentUser.username}!</h3>
+      <button className="login-buttons-btn" onClick={logout}>
+        Log Out
+      </button>
+    </span>
+  );
+  let loginButtons = currentUser ? personalGreeting() : sessionLinks();
 
-    <span className='links'>
-        <ul >
-            {/* <li>Discover</li> */}
-            
-            
-            {/* <li>Search Box</li>
+  return (
+    <div className={currentUser ? "nav-logged" : "nav-not-logged"}>
+      <span className="logo">
+        <h3 id="logo">
+          <Link to="/" id="logo">
+            MYpx
+          </Link>
+        </h3>
+      </span>
+
+      <span className="links">
+        <ul>
+          {/* <li>Discover</li> */}
+
+          {/* <li>Search Box</li>
             <li>Search Box</li>
             <li>Search Box</li> */}
         </ul>
+      </span>
 
-    </span>
-    
-       {loginButtons}
-            {/* <Link to="/login">Login</Link>
+      {loginButtons}
+      {/* <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
             <button className="header-button" onClick={logout}>Log Out</button> */}
-       
-        
-        
+    </div>
+  );
+};
 
-         
-    
-</div> )
-}
-
-export default NavBar
+export default NavBar;
--- frontend/components/posts/post_form.jsx
+++ frontend/components/posts/post_form.jsx
@@ -1,121 +1,120 @@
-import React from 'react'
-
-
+import React from "react";
 
 export default class PostForm extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            title: "",
-            category_id: "",
-            author_id: this.props.currentUser,
-            camera_name: ""
-        }
-        this.handleFile = this.handleFile.bind(this)
-        this.handleInput = this.handleInput.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-
-    }
-    handleInput(field) {
-        return e => this.setState({
-            [field]: e.target.value
-        })
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      category_id: "",
+      author_id: this.props.currentUser,
+      camera_name: ""
     };
+    this.handleFile = this.handleFile.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleInput(field) {
+    return e =>
+      this.setState({
+        [field]: e.target.value
+      });
+  }
 
-    componentWillUnmount() {
-        this.props.clearErrors();
-    }
+  componentWillUnmount() {
+    this.props.clearErrors();
+  }
 
-    handleFile(e) {
-        const file = e.currentTarget.files[0];
-        const fileReader = new FileReader();
-        fileReader.onloadend = () => {  
-         this.setState({ photoFile: file, photoUrl: fileReader.result });
+  handleFile(e) {
+    const file = e.currentTarget.files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      this.setState({ photoFile: file, photoUrl: fileReader.result });
     };
     if (file) {
-        fileReader.readAsDataURL(file);
+      fileReader.readAsDataURL(file);
     }
-}
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        
-        const formData = new FormData();
-        formData.append('post[title]', this.state.title)
-        formData.append('post[category_id]', this.state.category_id)
-        formData.append('post[author_id]', this.state.author_id)
-        formData.append('post[camera_name]', this.state.camera_name);
-        if (this.state.photoFile) {
+  handleSubmit(e) {
+    e.preventDefault();
 
-            formData.append('post[photo]', this.state.photoFile);
-        }
-        this.props.submitPost(formData)
-        .then(() => {
-             this.props.handleClose();
-             this.props.clearErrors();
-             this.setState({
-                title: "",
-                category_id: "",
-                author_id: this.props.currentUser,
-                camera_name: "",
-                photoFile:  undefined,
-                photoUrl: ""
-
-            })
-        }
-        , () => this.renderErrors())
-            
-        
-        
+    const formData = new FormData();
+    formData.append("post[title]", this.state.title);
+    formData.append("post[category_id]", this.state.category_id);
+    formData.append("post[author_id]", this.state.author_id);
+    formData.append("post[camera_name]", this.state.camera_name);
+    if (this.state.photoFile) {
+      formData.append("post[photo]", this.state.photoFile);
     }
-    renderErrors() {
-        return (
-            <ul>
-                {this.props.errors.map((error, i) => (
-                    <li key={`error-${i}`}>
-                        {error}
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+    this.props.submitPost(formData).then(
+      () => {
+        this.props.handleClose();
+        this.props.clearErrors();
+        this.setState({
+          title: "",
+          category_id: "",
+          author_id: this.props.currentUser,
+          camera_name: "",
+          photoFile: undefined,
+          photoUrl: ""
+        });
+      },
+      () => this.renderErrors()
+    );
+  }
+  renderErrors() {
+    return (
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>{error}</li>
+        ))}
+      </ul>
+    );
+  }
 
+  render() {
+    const preview = this.state.photoUrl ? (
+      <img className="img-preview" src={this.state.photoUrl} />
+    ) : null;
+    return (
+      <div className="form-container">
+        <form className="form-itself">
+          <label htmlFor="title">
+            Post Title
+            <input
+              type="text"
+              id="title"
+              value={this.state.title}
+              onChange={this.handleInput("title")}
+            />
+          </label>
 
-
-    render() {
-       
-        const preview = this.state.photoUrl ? <img className='img-preview' src={this.state.photoUrl} /> : null;
-        return (
-            <div className="form-container">
-                <form className='form-itself' onSubmit={this.handleSubmit}>
-                    <label htmlFor="title">Title     
-                        <input type="text"
-                        id="title"
-                        value={this.state.title}
-                        onChange={this.handleInput('title')} />
-                    </label>
-                    {/* <label>Category
+          {/* <label>Category
                         <input type="number"
                         // id="post-body"
                         value={this.state.category_id}
                         onChange={this.handleInput('category_id')} />
                     </label> */}
-                    {/* <label>Camera
+          {/* <label>Camera
                         <input type="text"
                         value={this.state.camera_name}
                         onChange={this.handleInput('camera_name')} />
                     </label> */}
-                    <label >
-                        <input type="file"
-                            onChange={this.handleFile} />
-                        </label>
-                        {this.renderErrors()}
-                    <h3>Image preview </h3>
-                    {/* <button onClick={this.props.handleClose}>Add Photo</button> */}
-                    <input type="submit" value="add Photo"/>
-                    {preview}
-                </form>
-            </div>
-        );
-    }
+          <label>
+            <input type="file" onChange={this.handleFile} />
+          </label>
+          {this.renderErrors()}
+          <br />
+          <button className="comment-submit-btn" onClick={this.handleSubmit}>
+            {" "}
+            Submit
+          </button>
+        </form>
+        <div className="modal-image-preview">
+          <h3>Image preview </h3>
+          {preview}
+        </div>
+      </div>
+    );
+  }
 }
