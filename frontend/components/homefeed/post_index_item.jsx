4c271556b39bf49428134422e59e4b17eb0b5102
msheezi <marshall.randolph@gmail.com>
2019-08-21 14:18:26 -0700

    update index layout

--- frontend/components/homefeed/post_index_item.jsx
+++ frontend/components/homefeed/post_index_item.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+// const myRedirect = ({post}) => {
+
+// }
+
 const PostIndexItem = ({ post,  props }) => {
+  
+  
+  
   return (
     // <li className="photo-post-index">
+    <span>
+
         <Link to={`/posts/${post.id}`} className="photo-index-title">
            <img className="photo-post-index-photo" src={post.photoUrl} />
       </Link> 
+    </span>
+
+
+    
+      // <img className="photo-post-index-photo" src={post.photoUrl} />
+    
+
 
     // </li>
   );
