d575e4cd6fb87e029e8d4e7d0d7b081fbf1b62d3
Msheezi <marshall.randolph@gmail.com>
2019-08-23 18:11:49 -0700

    Add Cloudinary integration

--- frontend/components/homefeed/post_index_item.jsx
+++ frontend/components/homefeed/post_index_item.jsx
@@ -1,21 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 
-
-
-
-const PostIndexItem = ({ post,  props }) => {
-  
-  let postId = post.id
-  
+const PostIndexItem = ({ post, props }) => {
+  // const imgRef = useRef(null);
+  let postId = post.id;
+  let betterUrl = post.photoUrl.split("?")[0];
+  // console.log(imgRef);
   return (
-    
-      
-        <img className="photo-post-index-photo" src={post.photoUrl} onClick={(e,postId) => props.history.push(`/posts/${post.id}`)}/>
-     
-    
-    
-   
+    <img
+      ref={imgRef => console.log(imgRef && imgRef.naturalWidth)}
+      className="photo-post-index-photo"
+      src={`https://res.cloudinary.com/ddtykf72z/image/fetch/h_300,w_400,c_fill/${betterUrl}`}
+      onClick={(e, postId) => props.history.push(`/posts/${post.id}`)}
+    />
   );
 };
 
