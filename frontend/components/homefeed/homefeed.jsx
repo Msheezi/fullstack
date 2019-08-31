f0f170dae49c9f0f90a7fea8246c77b934bdeecf
Msheezi <marshall.randolph@gmail.com>
2019-08-31 15:29:20 -0400

    Layout Function stuff

--- frontend/components/homefeed/homefeed.jsx
+++ frontend/components/homefeed/homefeed.jsx
@@ -11,38 +11,48 @@ class Home extends React.Component {
     super(props);
     this.state = {
       loaded: false,
+      imgDims: [],
       test: [["1"], ["2", "3"], ["4"]]
     };
 
-    this.getImgValues = this.getImgValues.bind(this)
+    this.getImgValues = this.getImgValues.bind(this);
   }
 
   componentDidMount() {
-    this.props
-      .fetchPosts()
-      .then(() => this.props.fetchUsers()).then(() => this.props.posts.map(post => this.getImgValues(img, post.photoUrl)))
-      .then(() =>
-        this.setState({
-          loaded: true
-        })
-      );
+    let myImg = new Image();
+    this.props.fetchPosts().then(() => this.props.fetchUsers());
 
+    // .then(() =>
+    //   this.setState({
+    //     loaded: true
+    //   })
+    // );
 
-      
+    // .then(() =>
+    //   this.props.posts.map(post => this.getImgValues(myImg, post.photoUrl))
+    // )
+    let imgDimArray = this.props.posts.map(post =>
+      this.getImgValues(myImg, post.photoUrl)
+    );
+
+    this.setState({
+      imgDims: imgDimArray,
+      loaded: true
+    });
+
+    console.log(imgDimArray);
   }
 
-
   getImgValues(img, src) {
-    let myImg = new Image()
-    debugger
+    // debugger;
     return new Promise((resolve, reject) => {
-      myImg.src = src
-      myImg.addEventListener("load", e => {
-        resolve(img)
-        
-                console.log(img)
-      })
-    })
+      img.src = src;
+      img.addEventListener("load", e => {
+        resolve(img);
+
+        console.log(img.naturalHeight);
+      });
+    });
   }
 
   render() {
@@ -59,16 +69,27 @@ class Home extends React.Component {
     //   </div>
     // );
 
+    // this.props.posts.forEach(post => console.log(this.getImgValues(new Image(), post.photoUrl)))
     // this.props.posts.map(post => { return new Promise((resolve, reject) => { post.addEventListner("load", e => { resolve(post) }) }) })
 
     // this.props.posts.map( img,post => )
     // this 2D array is the layout of the image used from the function you haven't written yet
 
-    
-    
+    // let myImg = new Image()
+    // let myFunc = (img, src) => {
+    //   return new Promise((resolve, reject) => {
+    //     myImg.src = src
+    //     myImg.addEventListener("load", e => {
+    //       resolve(img)
+    //     })
+    //   })
+    // }
+    // myFunc(myImg, "https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg").then(loadedImg => {
+    //   debugger
+    // })
 
     // this function below is a sample of what you can use to get the image proportions
-    // 
+    //
     // let myFunc = (img, src) => {
     //   return new Promise((resolve, reject) => {
     //     myImg.src = src
--- frontend/components/homefeed/post_index_item.jsx
+++ frontend/components/homefeed/post_index_item.jsx
@@ -8,7 +8,7 @@ const PostIndexItem = ({ post, props }) => {
   // console.log(imgRef);
   return (
     <img
-      ref={imgRef => console.log(imgRef && imgRef.naturalWidth)}
+      // ref={imgRef => console.log(imgRef && imgRef.naturalWidth)}
       className="photo-post-index-photo"
       src={`https://res.cloudinary.com/ddtykf72z/image/fetch/w_500/${betterUrl}`}
       onClick={e => props.history.push(`/posts/${post.id}`)}
