8e49c3ed4be32f2a23ecabe56a5497a1762d7bbb
Msheezi <marshall.randolph@gmail.com>
2019-09-05 18:30:16 -0700

    Update flow through phm

--- frontend/components/posts/photo_manager.jsx
+++ frontend/components/posts/photo_manager.jsx
@@ -1,138 +1,137 @@
-import React from 'react'
-import { connect } from 'react-redux';
-import PhotoManagerItem from './photo_manager_item'
-import { fetchAllPosts, deletePost, updatePost } from "../../actions/posts_actions";
+import React from "react";
+import { connect } from "react-redux";
+import PhotoManagerItem from "./photo_manager_item";
+import {
+  fetchAllPosts,
+  deletePost,
+  updatePost
+} from "../../actions/posts_actions";
 import { openModal } from "../../actions/ui_actions";
-import PostFormEdit from './photo_manager_edit'
+import PostFormEdit from "./photo_manager_edit";
+import { fetchAllUsers } from "../../actions/user_actions";
 
+class PhotoManager extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photoSelected: false,
+      loaded: false,
+      post: null
+    };
 
-class PhotoManager extends React.Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            photoSelected: false,
-            loaded: false,
-            post: null
-            
-            }
+    this.handlePhotoDelete = this.handlePhotoDelete.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchPosts().then(() =>
+      this.setState({
+        loaded: true
+      })
+    );
+  }
+
+  handlePhotoSelect(e, post) {
+    // debugger
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({ post: post, photoSelected: true });
+    // console.log(this.state.post)
+  }
+
+  handlePhotoDelete(postId) {
+    // e.preventDefault();
+    // e.stopPropagation();
+    this.setState({ post: "", photoSelected: false, loaded: false }, () =>
+      this.props
+        .deletePost(postId)
+        .then(() => this.props.fetchAllUsers())
+        .then(() => this.setState({ loaded: true }))
+    );
+  }
+
+  renderUpdateForm() {
+    if (this.state.photoSelected) {
+      return (
+        <PostFormEdit
+          post={this.state.post}
+          updatePost={this.props.updatePost}
+          deletePost={this.props.deletePost}
+          handlePhotoDelete={this.handlePhotoDelete}
+        />
+      );
+    } else {
+      return <PostFormEdit post={{ title: "" }} />;
     }
+  }
 
-    componentDidMount() {
-        this.props.fetchPosts() 
-        .then(() => this.setState({
-            loaded: true
-        })
-        )
+  render() {
+    // debugger;
+    if (this.state.loaded) {
+      console.log(this.props.posts);
+      let posts = this.props.posts
+        .map(post => (
+          <PhotoManagerItem
+            key={post.id}
+            post={post}
+            deletePost={this.props.deletePost}
+            props={this.props}
+            handlePhotoSelect={e => this.handlePhotoSelect(e, post)}
+          />
+        ))
+        .reverse();
+
+      return (
+        <div className="manager-page">
+          <div className="manager-title">
+            <span className="manager-button">
+              <button
+                type="button"
+                className="man-button"
+                onClick={this.props.openModal}
+              >
+                Upload Photo
+              </button>
+            </span>
+            <span className="manager-title-title">Photo Library</span>
+          </div>
+          <div className="manager-container">
+            <div className="manager-left">
+              <br />
+              <span id="left-bar-title">Photos</span>
+              <br />
+
+              <br />
+              <span id="left-bar-detail">
+                Library {this.props.posts.length}
+              </span>
+            </div>
+            <div className="manager-middle">{posts}</div>
+            <div className="manager-right">{this.renderUpdateForm()}</div>
+          </div>
+        </div>
+      );
+    } else {
+      return "";
     }
-
-    handlePhotoSelect(e, post) {
-        // debugger
-        e.preventDefault();
-        e.stopPropagation()
-        this.setState({post: post, photoSelected: true})
-        // console.log(this.state.post)
-    }
-
-    // dataTest(){
-    //     if (this.state.photoSelected){
-    //         return this.state.post.title
-    //     }
-    // }
-
-    renderUpdateForm() {
-        if (this.state.photoSelected) {
-            return <PostFormEdit post={this.state.post} updatePost={this.props.updatePost} deletePost={this.props.deletePost} />;
-        } else {
-            return <PostFormEdit post={{  title: '' }} />;
-        }
-    }
-
-   
-
-    render() {
-        // debugger
-        if (this.state.loaded){
-            let posts = this.props.posts.map(post => (
-                <PhotoManagerItem
-                key={post.id}
-                post={post}
-                deletePost={this.props.deletePost}
-                props ={this.props}
-                handlePhotoSelect={ e => this.handlePhotoSelect(e,post)}
-                // onClick={(e, post) =>  this.photoSelected.bind(this,post)}
-                />
-                )).reverse();
-
-            // let posts = this.props.posts.map(post => {
-            //     let betterUrl = post.photoUrl.split("?")[0]
-            //     return (
-            //         <img 
-            //             key={post.id}
-            //             className="manager-photo-item"
-            //             src={`https://res.cloudinary.com/ddtykf72z/image/fetch/w_160/${betterUrl}`}
-            //             onClick={(e, post) => this.handlePhotoSelect(e, post) }/>
-            //     )
-            // } )
-
-            // { this.props.userPhotos.map(el => 
-            // <PhotoManagerItem key={el.id} 
-            // selected={this.state.selectedPhoto === el}
-            //  photo={el}
-            //   handlePhotoSelect={e => this.handlePhotoSelect(e, el)} />) }
-                // let data = this.state.photoSelected
-
-
-                return (
-                   <div className="manager-page">
-                        <div className="manager-title">
-                            <span className="manager-button">
-                                <button type="button" className="man-button" onClick={this.props.openModal}>
-                                    Upload Photo
-                                </button>
-                            </span> 
-                            <span className="manager-title-title">
-                                Photo Library
-                            </span>
-                        </div>
-                            <div className="manager-container">
-                            <div className="manager-left">
-                                <br />
-                                <span id="left-bar-title">Photos</span>
-                                <br/>
-                                
-                                <br />
-                                <span id="left-bar-detail">Library {this.props.posts.length}</span>
-                            </div>
-                            <div className="manager-middle">{posts}</div>
-                            <div className="manager-right"> 
-                                
-                                {this.renderUpdateForm()}
-                            </div>
-                        </div>
-                    </div> 
-        )
-            } else {
-                return ""
-            }
-    }
+  }
 }
-
-
 
 const mapStateToProps = state => {
-    let postIds = state.entities.users[state.session.id].post_ids
-    return {
-        posts: postIds.map(id => state.entities.posts[id])
-    }
-}
-    
+  let postIds = state.entities.users[state.session.id].post_ids;
+  return {
+    posts: postIds.map(id => state.entities.posts[id])
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
-    deletePost: postId => dispatch(deletePost(postId)),
-    fetchPosts: () => dispatch(fetchAllPosts()),
-    openModal: () => dispatch(openModal()),
-    updatePost: post => dispatch(updatePost(post))
+  deletePost: postId => dispatch(deletePost(postId)),
+  fetchPosts: () => dispatch(fetchAllPosts()),
+  openModal: () => dispatch(openModal()),
+  updatePost: post => dispatch(updatePost(post)),
+  fetchAllUsers: () => dispatch(fetchAllUsers())
+});
 
-})
-
-export default  connect(mapStateToProps, mapDispatchToProps)(PhotoManager)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PhotoManager);
--- frontend/components/posts/photo_manager_edit.jsx
+++ frontend/components/posts/photo_manager_edit.jsx
@@ -1,141 +1,133 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
-
 class PostFormEdit extends React.Component {
-    constructor(props) {
-        super(props);
-        // this.state = {
-        //     title: this.props.post.title,
-        //     category_id: "",
-        //     authorId: this.props.post.author_id,
-        //     cameraName: this.props.camera_name
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //     title: this.props.post.title,
+    //     category_id: "",
+    //     authorId: this.props.post.author_id,
+    //     cameraName: this.props.camera_name
 
-            
-            
-        // };
+    // };
 
-        this.state = this.props.post
-        // this.handleFile = this.handleFile.bind(this);
-        // this.handleInput = this.handleInput.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        // this.renderFileUpload = this.renderFileUpload.bind(this);
-        // this.renderUpload = this.renderUpload.bind(this);
-        // this.modalClose = this.modalClose.bind(this);
-        // this.handleDragEnter = this.handleDragEnter.bind(this);
-        // this.handleDragLeave = this.handleDragLeave.bind(this);
-        // this.handleDrop = this.handleDrop.bind(this);
+    this.state = this.props.post;
+    // this.handleFile = this.handleFile.bind(this);
+    // this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    // this.renderFileUpload = this.renderFileUpload.bind(this);
+    // this.renderUpload = this.renderUpload.bind(this);
+    // this.modalClose = this.modalClose.bind(this);
+    // this.handleDragEnter = this.handleDragEnter.bind(this);
+    // this.handleDragLeave = this.handleDragLeave.bind(this);
+    // this.handleDrop = this.handleDrop.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.post !== this.props.post) {
+      console.log(this.props.post);
+      this.setState(this.props.post);
     }
+  }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.post !== this.props.post) {
-            this.setState(this.props.post);
-        }
+  handleInput(field) {
+    return e =>
+      this.setState({
+        [field]: e.target.value
+      });
+  }
+
+  handleDelete(e) {
+    e.preventDefault();
+    // this.props.deletePost(this.state.id);
+    // this.setState({ id: "" });
+    this.props.handlePhotoDelete(this.state.id);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.updatePost(this.state);
+  }
+  // componentWillUnmount() {
+  //     this.props.clearErrors();
+  // }
+
+  renderEdit() {
+    // debugger;
+    if (this.state.id) {
+      return <h2 className="phm-edit-title">Editing 1 Photo</h2>;
+    } else {
+      return <h2 className="phm-edit-title">Edit</h2>;
     }
+  }
 
-    handleInput(field) {
-        return e =>
-            this.setState({
-                [field]: e.target.value
-            });
-    }
+  render() {
+    // debugger;
+    return (
+      <div>
+        {this.renderEdit()}
+        <form
+          onSubmit={this.handleSubmit}
+          className="photo-manager-form-container"
+        >
+          <label> Category</label>
 
-    handleDelete(e){
-        e.preventDefault()
-        this.props.deletePost(this.state.id)
-        this.setState({id: ""})
-    }
+          <input
+            className="phm-category-input"
+            type="text"
+            value={this.state.category_id || "Uncategorized"}
+            onChange={this.handleInput("category_id")}
+            // placeholder="Enter A Category"
+          />
 
-    handleSubmit(e){
-        
-        e.preventDefault()
-        this.props.updatePost(this.state)
+          <br />
+          <label> Title </label>
 
-    }
-    // componentWillUnmount() {
-    //     this.props.clearErrors();
-    // }
+          <input
+            className="phm-title-input"
+            type="text"
+            id="title"
+            value={this.state.title}
+            onChange={this.handleInput("title")}
+            // placeholder={}
+          />
 
-    
+          <br />
+          <label>Description </label>
+          <textarea
+            className="phm-desc-input"
+            value={this.state.desc}
+            onChange={this.handleInput("desc")}
+            id="desc"
+            placeholder="Tell us more about your beautiful photo"
+          />
+          <br />
 
-    renderEdit(){
-        if (this.state.id){
-            return  (
-                <h2 className="phm-edit-title">Editing 1 Photo</h2>
-                )
-        } else {
-            return <h2 className="phm-edit-title">Edit</h2>
-        }
-    }
+          <label> Camera </label>
 
-    render() {
-        // debugger
-        return (
-        <div>
-            {this.renderEdit()}
-                <form onSubmit={this.handleSubmit} className="photo-manager-form-container" >
-                    
-                    <label> Category</label>
-                        
-                        <input
-                            className="phm-category-input"
-                            type="text"
-                            value={this.state.category_id || "Uncategorized"}
-                            onChange={this.handleInput("category_id")}
-                            // placeholder="Enter A Category"
-                        />
-                    
-                    <br />
-                    <label> Title </label> 
-                        
-                        <input
-                            className="phm-title-input"
-                            type="text"
-                            id="title"
-                            value={this.state.title}
-                            onChange={this.handleInput("title")}
-                            // placeholder={}
-                        />
-                    
-                    <br />
-                    <label>Description </label>
-                        <textarea 
-                            className="phm-desc-input"
-                            value={this.state.desc}
-                            onChange={this.handleInput("desc")}
-                            id="desc"
-                            placeholder="Tell us more about your beautiful photo"
-                            />
-                    <br />
+          <input
+            className="phm-camera-input"
+            type="text"
+            value={this.state.cameraName}
+            onChange={this.handleInput("camera_name")}
+            // placeholder="Enter Camera Name"
+          />
 
-                    <label> Camera </label>
-                        
-                        <input
-                            className="phm-camera-input"
-                            type="text"
-                            value={this.state.cameraName}
-                            onChange={this.handleInput("camera_name")}
-                            // placeholder="Enter Camera Name"
-                        />
-                    
+          {/* {this.renderErrors()} */}
+          <br />
 
-                    {/* {this.renderErrors()} */}
-                    <br />
-                    
-                        <button className="phm-delete-btn" onClick={this.handleDelete}>
-                            Delete Photo
-                        </button>
-                        <button className="phm-submit-btn" onClick={this.handleSubmit}>
-                       
-                            Save
-                        </button>
-            
-            </form>
-        </div>)
-    }
-
-    
+          <button className="phm-delete-btn" onClick={this.handleDelete}>
+            Delete Photo
+          </button>
+          <button className="phm-submit-btn" onClick={this.handleSubmit}>
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default withRouter(PostFormEdit);
