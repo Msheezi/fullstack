9666766e00516aa3e09e5a08273f931a1ddd1905
msheezi <marshall.randolph@gmail.com>
2019-09-10 00:55:48 -0700

    Layout Adjustment

--- frontend/components/homefeed/post_index_item.jsx
+++ frontend/components/homefeed/post_index_item.jsx
@@ -5,6 +5,7 @@ const PostIndexItem = ({ post, props }) => {
   // const imgRef = useRef(null);
   // let postId = post.id;
   let betterUrl = post.photoUrl.split("?")[0];
+  let width = window.innerWidth / 4
   // console.log(imgRef);
   return (
     
@@ -13,7 +14,7 @@ const PostIndexItem = ({ post, props }) => {
       className="photo-post-index-photo"
       src={`https://res.cloudinary.com/ddtykf72z/image/fetch/h_400/${betterUrl}`}
       onClick={e => props.history.push(`/posts/${post.id}`)}
-      // style={{flexGrow: 1 , flexShrink: 1, flex: 400 }}
+      style={{flexGrow: 1 , flexShrink: 2, flexBasis: width }}
     />
     
   );
