05cb31d7daba37c128b1822b0f38128d4d8ab272
Msheezi <marshall.randolph@gmail.com>
2019-09-09 20:45:15 -0700

    Remove debugger, change username to first name

--- frontend/components/profile/user_profile_container.jsx
+++ frontend/components/profile/user_profile_container.jsx
@@ -1,30 +1,30 @@
-import {connect } from 'react-redux'
-import Profile from './user_profile'
-import { fetchAllPosts} from '../../actions/posts_actions'
-import {fetchAllUsers} from '../../actions/user_actions'
+import { connect } from "react-redux";
+import Profile from "./user_profile";
+import { fetchAllPosts } from "../../actions/posts_actions";
+import { fetchAllUsers } from "../../actions/user_actions";
 
+const mapStateToProps = (state, ownProps) => {
+  let user = ownProps.match.params.userId;
+  let allPosts = Object.keys(state.entities.posts).map(
+    id => state.entities.posts[id]
+  );
+  let posts = allPosts.filter(post => post.author_id == user);
+  let bgId = state.entities.users[user].bgphoto || 1;
 
-const mapStateToProps = (state , ownProps) => {
-    debugger
-    let user = ownProps.match.params.userId
-    let allPosts = Object.keys(state.entities.posts).map(id => state.entities.posts[id])
-    let posts = allPosts.filter(post => post.author_id == user )
-    let bgId = state.entities.users[user].bgphoto || 1
-
-    
-    let defaultBG = state.entities.posts[bgId].photoUrl
-   return {
+  let defaultBG = state.entities.posts[bgId].photoUrl;
+  return {
     user: state.entities.users[user],
     posts: posts,
     defaultBG: defaultBG
-
-   }
-    
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
-    fetchAllPosts: () => dispatch(fetchAllPosts()),
-    fetchAllUsers: () => dispatch(fetchAllUsers())
-})
+  fetchAllPosts: () => dispatch(fetchAllPosts()),
+  fetchAllUsers: () => dispatch(fetchAllUsers())
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Profile);
