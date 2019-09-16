865fa0169392524be7b6ca4be2a79c9b18593897
msheezi <marshall.randolph@gmail.com>
2019-09-16 14:26:14 -0700

    Add in gallery function for render

---
+++ frontend/components/homefeed/gallery_index_item.jsx
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+
+const GalleryIndexItem = ({ post, props }) => {
+  // const imgRef = useRef(null);
+  // let postId = post.id;
+  let betterUrl = post.photoUrl.split("?")[0];
+  let width = (window.innerWidth * 0.9) / 4;
+  // let myFlexGrow = imgRef.naturalWidth > width ? 2 : 1;
+
+  // console.log(imgRef);
+  return (
+    <img
+      // ref={imgRef =>
+      //   console.log(imgRef, imgRef.naturalWidth, "postid", post.id)
+      // }
+      className="photo-post-index-photo"
+      src={`https://res.cloudinary.com/ddtykf72z/image/fetch/c_fill,g_center,f_auto,h_400/${betterUrl}`}
+      onClick={e => props.history.push(`/posts/${post.id}`)}
+      style={{ flexGrow: 1, flexBasis: 175 }}
+    />
+  );
+};
+
+export default GalleryIndexItem;
--- frontend/components/homefeed/homefeed.jsx
+++ frontend/components/homefeed/homefeed.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PostIndexItem from "./post_index_item";
+import GalleryIndexItem from './gallery_index_item'
 import { connect } from "react-redux";
 
 import { fetchAllPosts, deletePost } from "../../actions/posts_actions";
@@ -27,8 +28,8 @@ class Home extends React.Component {
     this.props
       .fetchPosts()
       .then(() => this.props.fetchUsers())
-      .then(()=> this.props.fetchAllCategories())
       .then(()=> this.props.fetchAllGalleries())
+      .then(()=> this.props.fetchAllCategories())
     
       .then(() =>
         this.setState({
@@ -52,24 +53,45 @@ class Home extends React.Component {
     })
   }
 
+  renderPosts(){
+
+    let posts = this.props.posts
+      .map(post => (
+        <PostIndexItem
+          key={post.id}
+          post={post}
+          deletePost={this.props.deletePost}
+          props={this.props}
+        />
+      ))
+      .reverse();
+      return (
+
+        <div className="photo-index-container">{posts}</div>
+      )
+  }
+
+  renderGalleries(){
+    let galleries = this.props.galleries.map(gallery =>(
+      <GalleryIndexItem
+        key={gallery.id}
+        gallery={gallery}
+        props={this.props}
+        />
+    ))
+    return (
+      <div className = "gallery-index-container">{galleries}</div>
+    )
+  }
  
 
   render() {
     // debugger;
    
     if (this.state.loaded) {
-      let posts = this.props.posts
-        .map(post => (
-          <PostIndexItem
-            key={post.id}
-            post={post}
-            deletePost={this.props.deletePost}
-            props={this.props}
-          />
-        ))
-        .reverse();
       
-      return (
+      if (this.state.displayPosts){
+         return (
         <div className="index-container">
           <div className="index-title">
             <h2>What's popular today</h2>
@@ -81,19 +103,45 @@ class Home extends React.Component {
             <span className="select-galleries-index" onClick={this.displayGalleries}>Galleries</span>
           </div>
           <div className="index-photo-wrapper">
-          <div className="photo-index-container">{posts}</div>
+            {this.renderPosts()}
           </div>
         </div>
       );
-    } else {
-      return "";
+      } else {
+        return (
+          <div className="index-container">
+            <div className="index-title">
+              <h2>What's popular today</h2>
+              <p>See recently added photos and galleries below.</p>
+            </div>
+            <div className="photo-gallery-pane-selector1">
+              <span className="selector-photos-index" onClick={this.displayPosts}>Photos</span>
+              <span className="selector-spacer-index"></span>
+              <span className="select-galleries-index" onClick={this.displayGalleries}>Galleries</span>
+            </div>
+            <div className="index-photo-wrapper">
+              {this.renderGalleries()}
+            </div>
+          </div>
+        );
+
+      }
+     
+    } else  {
+      return ""
     }
   }
 }
 
-const mapStateToProps = state => ({
-  posts: Object.keys(state.entities.posts).map(id => state.entities.posts[id])
-});
+const mapStateToProps = state => {
+
+  
+  return {
+    galleries: Object.keys(state.entities.galleries).map(id =>state.entities.galleries[id]),
+   posts: Object.keys(state.entities.posts).map(id => state.entities.posts[id])
+}
+  
+};
 
 const mapDispatchToProps = dispatch => ({
   fetchPosts: () => dispatch(fetchAllPosts()),
