c4829d0a6570b32d450024e043333424fa6653c6
Msheezi <marshall.randolph@gmail.com>
2019-09-17 18:54:04 -0700

    fix default profile background pic logic

--- frontend/components/profile/user_profile_container.jsx
+++ frontend/components/profile/user_profile_container.jsx
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import Profile from "./user_profile";
 import { fetchAllPosts } from "../../actions/posts_actions";
 import { fetchAllUsers } from "../../actions/user_actions";
-import {fetchAllGalleries} from '../../actions/gallery_actions'
+import { fetchAllGalleries } from "../../actions/gallery_actions";
 
 const mapStateToProps = (state, ownProps) => {
   let user = ownProps.match.params.userId;
@@ -10,11 +10,13 @@ const mapStateToProps = (state, ownProps) => {
     id => state.entities.posts[id]
   );
   let posts = allPosts.filter(post => post.author_id == user);
-  let bgId = state.entities.users[user].bgphoto || 1;
-    let defaultBG = state.entities.posts[bgId].photoUrl
+  let bgId = state.entities.users[user].bgphoto || allPosts[0].id;
+  let defaultBG = state.entities.posts[bgId].photoUrl;
 
-   return {
-     galleries: Object.keys(state.entities.galleries).map(id => state.entities.galleries[id]),
+  return {
+    galleries: Object.keys(state.entities.galleries).map(
+      id => state.entities.galleries[id]
+    ),
     user: state.entities.users[user],
     posts: posts,
     defaultBG: defaultBG
@@ -25,7 +27,6 @@ const mapDispatchToProps = dispatch => ({
   fetchAllPosts: () => dispatch(fetchAllPosts()),
   fetchAllUsers: () => dispatch(fetchAllUsers()),
   fetchAllGalleries: () => dispatch(fetchAllGalleries())
-
 });
 
 export default connect(
